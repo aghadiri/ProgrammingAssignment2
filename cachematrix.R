fbc62c5931d8d65d0c1105cfcca406901c955d80
aghadiri <aghadiri@sce.carleton.ca>
2014-04-23 19:48:58 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions provide the oppotunity to skip unecessary calculations.
+## More specifically, if the inverse of a given matrix is calculated once,
+## it is stored in cache. Prospective repetitions of the same calculation 
+## is simply skipped, and instead the calcualted value is retrieved.
 
-## Write a short comment describing this function
+## makeCacheMatrix create a special matrix which is list whose components are:
+## set value of the matrix
+## set value of the inverse
+## get value of the matrix
+## get value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(theMat) {
+      x <<- theMat
+      inv <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    list (set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## casheSolve calcualtes and returns the inverse of a given matrix.
+## if the inverse is already calculated and stored in the cache,
+## it simply skips the calculation and returns the existing inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    if(!is.null(inv)) {
+        message("using cached data...")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setInverse(inv)
+    inv
 }
+
+## Used for testing
+## X = matrix(c(4,6,3,6,4,6,6,3,5), 3, 3)
+## sX <- makeCacheMatrix(X)
+## cacheSolve(sX)
+## cacheSolve(sX)
